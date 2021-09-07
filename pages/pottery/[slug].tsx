f5cbf78d3ef684c94f43064610945016269f3ba6
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2021-09-06 21:03:19 -0500

    started enabling ts strict mode

--- pages/pottery/[slug].tsx
+++ pages/pottery/[slug].tsx
@@ -1,20 +1,21 @@
 import { getClient } from '../../utils/sanity';
 import { getPotteryPathsQuery, getPotteryPropsQuery } from '../../utils/queries';
 import ShopPagePreviewWrapper from '../../components/ShopPagePreviewWrapper';
+import { GetStaticPaths, GetStaticProps } from 'next';
 
 export default function PotteryPageContainer({ pottery, preview, params }) {
   return <ShopPagePreviewWrapper preview={preview} query={getPotteryPropsQuery} shopData={pottery} params={params} />
 }
 
-export async function getStaticProps({ params, preview = false }) {
-  const pottery = await getClient(preview).fetch(getPotteryPropsQuery, { slug: params.slug })
+export const getStaticProps: GetStaticProps = async ({ params, preview = false }) => {
+  const pottery = await getClient(preview).fetch(getPotteryPropsQuery, { slug: params?.slug });
   return {
     props: { preview, pottery, params },
     revalidate: 1
   }
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const slugs = await getClient().fetch(getPotteryPathsQuery)
   return {
     paths: slugs.map(slug => ({ params: { slug } })),
