e7bb8e8bab0d917bf624c09b9309d47bf6e8adc5
Jeremiah Brandt <jeremiahjbrandt@outlook.com>
2021-09-23 13:55:34 -0500

    Added enabled property to queries

--- utils/queries.ts
+++ utils/queries.ts
@@ -1,13 +1,13 @@
 import { groq } from "next-sanity"
 
-export const getAllPaintingsQuery = `*[_type == "painting" && defined(slug.current)]`
-export const getPaintingPropsQuery = groq`*[_type == "painting" && slug.current == $slug][0]`
-export const getPaintingPathsQuery = groq`*[_type == "painting" && defined(slug.current)][].slug.current`
+export const getAllPaintingsQuery = `*[_type == "painting" && defined(slug.current) && enabled == true]`
+export const getPaintingPropsQuery = groq`*[_type == "painting" && slug.current == $slug && enabled == true][0]`
+export const getPaintingPathsQuery = groq`*[_type == "painting" && defined(slug.current) && enabled == true][].slug.current`
 
-export const getAllPotteryQuery = `*[_type == "pottery" && defined(slug.current)]`
-export const getPotteryPropsQuery = groq`*[_type == "pottery" && slug.current == $slug][0]`
-export const getPotteryPathsQuery = groq`*[_type == "pottery" && defined(slug.current)][].slug.current`
+export const getAllPotteryQuery = `*[_type == "pottery" && defined(slug.current) && enabled == true]`
+export const getPotteryPropsQuery = groq`*[_type == "pottery" && slug.current == $slug && enabled == true][0]`
+export const getPotteryPathsQuery = groq`*[_type == "pottery" && defined(slug.current) && enabled == true][].slug.current`
 
-export const getAllPhotographyQuery = `*[_type == "photography" && defined(slug.current)]`
-export const getPhotographyPropsQuery = groq`*[_type == "photography" && slug.current == $slug][0]`
-export const getPhotographyPathsQuery = groq`*[_type == "photography" && defined(slug.current)][].slug.current`
+export const getAllPhotographyQuery = `*[_type == "photography" && defined(slug.current) && enabled == true]`
+export const getPhotographyPropsQuery = groq`*[_type == "photography" && slug.current == $slug && enabled == true][0]`
+export const getPhotographyPathsQuery = groq`*[_type == "photography" && defined(slug.current) && enabled == true][].slug.current`
