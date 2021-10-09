f7daa85d786daa1ea2991f26a39c2d016fc83bf7
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2021-10-09 11:31:29 -0500

    changed test environment for about and contact pages

--- pages/about.test.tsx
+++ pages/about.test.tsx
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { screen, render } from '@testing-library/react'
 import AboutPage from './about'
 
--- pages/contact.test.tsx
+++ pages/contact.test.tsx
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { screen, render } from '@testing-library/react'
 import ContactPage from './contact'
 
