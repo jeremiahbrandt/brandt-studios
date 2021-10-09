5894441b94c653512ee39d04910d1c8facea4e42
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2021-10-09 10:58:43 -0500

    Wrapped about and contact page tests in describe

--- pages/about.test.tsx
+++ pages/about.test.tsx
@@ -1,13 +1,16 @@
 import { screen, render } from '@testing-library/react'
 import AboutPage from './about'
 
-test('renders about page', () => {
-  // Arrange
-  expect.assertions(1)
 
-  // Act
-  render(<AboutPage />)
-
-  // Assert
-  expect(screen.getByText('This is the about page')).toBeInTheDocument()
+describe('aboutPage tests', () => {
+  it('renders about page', () => {
+    // Arrange
+    expect.assertions(1)
+  
+    // Act
+    render(<AboutPage />)
+  
+    // Assert
+    expect(screen.getByText('This is the about page')).toBeInTheDocument()
+  })
 })
--- pages/contact.test.tsx
+++ pages/contact.test.tsx
@@ -1,13 +1,15 @@
 import { screen, render } from '@testing-library/react'
 import ContactPage from './contact'
 
-test('renders contact page', () => {
-  // Arrange
-  expect.assertions(1)
-
-  // Act
-  render(<ContactPage />)
-
-  // Assert
-  expect(screen.getByText('This is the contact page.')).toBeInTheDocument()
+describe('contactPage tests', () => {
+  it('renders contact page', () => {
+    // Arrange
+    expect.assertions(1)
+  
+    // Act
+    render(<ContactPage />)
+  
+    // Assert
+    expect(screen.getByText('This is the contact page.')).toBeInTheDocument()
+  })
 })
